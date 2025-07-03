d4a5d5dd7a3f4c43ef3ff5ed55a67678b3d4ce70
Hamz04 <officialhamza16@gmail.com>
2025-07-03 23:18:06 +0000

    config: add hardhat with sepolia network

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: { optimizer: { enabled: true, runs: 200 } }
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111
+    }
+  },
+  etherscan: { apiKey: process.env.ETHERSCAN_API_KEY }
+};
